ed89a369642392f0d8f062b50d81fea2de073788
ZUO H.L <60392737+daniel620@users.noreply.github.com>
2022-08-16 16:05:10 +0800

    Update UI

--- JHU-Gait-WatchApp/ContentView.swift
+++ JHU-Gait-WatchApp/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("The sandbox of watch app")
             .padding()
+//        Text("Hello, world!")
+//            .padding()
     }
 }
 
